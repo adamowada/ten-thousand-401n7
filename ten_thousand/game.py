0c083f9f4860bd3570b9382d831967f46aae4c92
Adam Owada <adamowada@gmail.com>
2023-04-11 17:18:25 -0700

    ten thousand 1 tests passing

---
+++ ten_thousand/game.py
@@ -0,0 +1,10 @@
+from game_logic import GameLogic
+
+
+def play():
+    pass
+
+
+if __name__ == "__main__":
+    play()
+
---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,49 @@
+from collections import Counter
+import random
+
+
+class GameLogic:
+    @staticmethod
+    def roll_dice(n):
+        """
+        Rolls n 6-sided dice.
+        :param n: Number of dice.
+        :return: Tuple of dice rolls.
+        """
+        rolls = tuple(random.randint(1, 6) for _ in range(n))
+        return rolls
+
+    @staticmethod
+    def calculate_score(roll):
+        """
+        Scores a give roll according to the rules of Ten Thousand.
+        :param roll: A tuple of integers representing dice values.
+        :return: An integer representing the score.
+        """
+        roll = Counter(roll)
+        score = 0
+
+        # Check for straight
+        if len(roll) == 6:
+            return 1500
+
+        # Check for 3 pair
+        if len(roll) == 3 and roll.most_common()[0][1] == 2:
+            return 1500
+
+        # 3 or more of any number
+        for i in range(1, 7):
+            quantity = roll[i]
+            if quantity >= 3:
+                if i == 1:
+                    score += (quantity - 2) * 1000
+                else:
+                    score += (quantity - 2) * i * 100
+
+        # 1s and 5s
+        score += 100 if roll[1] == 1 else 0
+        score += 200 if roll[1] == 2 else 0
+        score += 50 if roll[5] == 1 else 0
+        score += 100 if roll[5] == 2 else 0
+
+        return score
