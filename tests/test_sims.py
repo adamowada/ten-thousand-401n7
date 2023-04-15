101c70b725b96b1e9e3169368f42c54823529d9e
Adam Owada <adamowada@gmail.com>
2023-04-15 01:00:53 -0700

    "Refactored get_inputs function to take list of lines as input and improved documentation."

--- tests/test_sims.py
+++ tests/test_sims.py
@@ -3,14 +3,19 @@ from ten_thousand.game_logic import GameLogic
 from ten_thousand.game import play
 
 
-def get_inputs(text):
-    """Takes text from a sim file and returns a list of strings, representing inputs"""
-    text = text.split("\n")
-    for line in text:
-        if line.startswith("> ")
+def get_inputs(lines):
+    """
+    Get the sim file's inputs
+    :param text: List of lines from sim text's .readlines().
+    :return: List of inputs to mock.
+    """
+    inputs = []
+    for line in lines:
+        if line.startswith("> "):
+            
 
 
 def test_quitter(monkeypatch, capsys):
     with open("quitter.sim.txt", "r") as f:
-        text = f.read()
+        inputs = get_inputs(f.readlines())
 
