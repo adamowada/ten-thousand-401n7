4604f05ab55753d92993ec152bcb1b571f9aaeb8
Adam Owada <adamowada@gmail.com>
2023-04-15 02:41:03 -0700

    "Refactored test_sims.py to include get_mock_rolls function and updated test_input filenames."

--- tests/test_sims.py
+++ tests/test_sims.py
@@ -11,34 +11,24 @@ def get_inputs(lines):
     return inputs
 
 
-# def test_quitter(monkeypatch, capsys):
-#     with open("tests/quitter.sim.txt", "r") as f:
-#         lines = f.readlines()
-#         inputs = get_inputs(lines)
-#
-#     def mock_input(prompt):
-#         print(prompt, inputs[0], sep="")
-#         return inputs.pop(0)
-#
-#     monkeypatch.setattr("builtins.input", mock_input)
-#     play()
-#     captured = capsys.readouterr().out.replace("\n\n", "\n")  # jank
-#     output_lines = captured.split("\n")
-#     for i, v in enumerate(lines):
-#         assert v.strip() == output_lines[i]
+def get_mock_rolls(lines)
+    mock_rolls = []
+    for line in lines:
+        if line.startswith("*** "):
+            mock_rolls.append(tuple(int(i) for i in line if ))
 
 
 @pytest.mark.parametrize(
     "test_input",
     [
-        ("tests/bank_first_for_two_rounds.sim"),
-        ("tests/bank_one_roll_then_quit.sim"),
-        ("tests/cheat_and_fix.sim"),
-        ("tests/hot_dice.sim"),
-        ("tests/one_and_done.sim"),
-        ("tests/quitter.sim"),
-        ("tests/repeat_roller.sim"),
-        ("tests/zilcher.sim"),
+        ("tests/bank_first_for_two_rounds.sim.txt"),
+        ("tests/bank_one_roll_then_quit.sim.txt"),
+        ("tests/cheat_and_fix.sim.txt"),
+        ("tests/hot_dice.sim.txt"),
+        ("tests/one_and_done.sim.txt"),
+        ("tests/quitter.sim.txt"),
+        ("tests/repeat_roller.sim.txt"),
+        ("tests/zilcher.sim.txt"),
     ],
 )
 def test_all(monkeypatch, capsys, test_input):
