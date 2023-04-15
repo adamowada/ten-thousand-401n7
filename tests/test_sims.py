ae19f8f5bb9949b5405a30538618e90ada60ba11
Adam Owada <adamowada@gmail.com>
2023-04-15 00:54:49 -0700

    "Added get_inputs() function and updated test_quitter() in test_sims.py file"

--- tests/test_sims.py
+++ tests/test_sims.py
@@ -3,7 +3,12 @@ from ten_thousand.game_logic import GameLogic
 from ten_thousand.game import play
 
 
-def test_quitter():
+def get_inputs(text):
+    """Takes text from a sim file and returns a list of strings, representing inputs"""
+    text = split()
+
+
+def test_quitter(monkeypatch, capsys):
     with open("quitter.sim.txt", "r") as f:
         text = f.read()
-    
+
